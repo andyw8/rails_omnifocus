74425abe3abb1c8e2249eac78cf1db5e67f82373
Andy Waite <13400+andyw8@users.noreply.github.com>
2025-05-25 23:36:25 -0400

    Task association

--- app/models/project_info.rb
+++ app/models/project_info.rb
@@ -4,6 +4,11 @@ class ProjectInfo < ApplicationRecord
   self.table_name = "ProjectInfo"
   has_many :tasks, dependent: :destroy, foreign_key: "projectinfo"
   belongs_to :team
+  belongs_to :task, foreign_key: :task
   has_many :folders, through: :tasks
   has_many :next_tasks, class_name: "Task", foreign_key: "nextTask"
+
+  def name
+    task.name
+  end
 end
